4542d2f4cf2df69ea8bb33c92084878e70cb673e
Matias <hormaza1992@gmail.com>
2024-10-29 17:28:58 +0100

    format code. Remove unused import

--- woonuxt_base/app/plugins/Gql2.ts
+++ woonuxt_base/app/plugins/Gql2.ts
@@ -1,4 +1,3 @@
-import stockData from '../data/stockStatus.json';
 import getProducts from '../data/getProducts.json';
 import getProductsCategories from '../data/getProductsCategories.json';
 import getCart from '../data/getCart.json';
@@ -8,57 +7,57 @@ import updateCartQuantity from '../data/updateCartQuantity.json';
 import emptyCart from '../data/emptyCart.json';
 
 export default defineNuxtPlugin(() => {
-    return {
-        provide: {
-            useGql2: (methodName: string, params: any) => {
-                console.log('useGql2:', params, methodName);
-                switch (methodName) {
-                    case 'getProduct':
-                        return {
-                            "data": {
-                                "value": {
-                                    "product": getProducts.data.value.products.nodes.find((product) => product.slug === params.slug)
-                                }
-                            }                      
-                        };
+  return {
+    provide: {
+      useGql2: (methodName: string, params: any) => {
+        console.log('useGql2:', params, methodName);
+        switch (methodName) {
+          case 'getProduct':
+            return {
+              data: {
+                value: {
+                  product: getProducts.data.value.products.nodes.find((product) => product.slug === params.slug),
+                },
+              },
+            };
 
-                    case 'getProducts':
-                        return getProducts;
+          case 'getProducts':
+            return getProducts;
 
-                    case 'getStockStatus':
-                        return {
-                            "data": {
-                                "product": {
-                                    "stockStatus": getProducts.data.value.products.nodes.find((product) => product.slug === params.slug)?.stockStatus ?? 'IN_STOCK'
-                                }
-                            }
-                        };
+          case 'getStockStatus':
+            return {
+              data: {
+                product: {
+                  stockStatus: getProducts.data.value.products.nodes.find((product) => product.slug === params.slug)?.stockStatus ?? 'IN_STOCK',
+                },
+              },
+            };
 
-                    case 'getProductCategories':
-                        return getProductsCategories;
+          case 'getProductCategories':
+            return getProductsCategories;
 
-                    case 'getCart':
-                        return getCart;
+          case 'getCart':
+            return getCart;
 
-                    case 'getAllTerms':
-                        return getAllTerms;
+          case 'getAllTerms':
+            return getAllTerms;
 
-                    case 'addToCart':
-                        console.log('addTocart', params);
-                        return addToCart;
-                        
-                    case 'updateCartQuantity':
-                        console.log('updateCartQuantity', params);
-                        return updateCartQuantity;
+          case 'addToCart':
+            console.log('addTocart', params);
+            return addToCart;
 
-                    case 'emptyCart':
-                        console.log('Empty cart');
-                        return emptyCart;
-                    
-                    default:
-                        return console.log('Method not found');
-                }
-            }
+          case 'updateCartQuantity':
+            console.log('updateCartQuantity', params);
+            return updateCartQuantity;
+
+          case 'emptyCart':
+            console.log('Empty cart');
+            return emptyCart;
+
+          default:
+            return console.log('Method not found');
         }
-    }
-})
+      },
+    },
+  };
+});
