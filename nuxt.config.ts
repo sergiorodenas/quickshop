de36a5e41efc849a65624ca89c382dce83e217b0
Sergio Rodenas <sergiorodenasgomez@gmail.com>
2025-03-14 12:33:33 +0000

    Add https://nuxt.com/modules/stripe-next

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,4 +21,24 @@ export default defineNuxtConfig({
       failOnError: false,
     },
   },
+
+  modules: ['@unlok-co/nuxt-stripe'],
+
+  stripe: {
+    // Server
+    server: {
+      key: process.env.STRIPE_SECRET_KEY,
+      options: {
+        // your api options override for stripe server side
+        // https://github.com/stripe/stripe-node?tab=readme-ov-file#configuration
+      },
+      // CLIENT
+    },
+    client: {
+      key: process.env.STRIPE_PUBLIC_KEY,
+      // manualClientLoad: true, // if you want to have control where you are going to load the client
+      // your api options override for stripe client side https://stripe.com/docs/js/initializing#init_stripe_js-options
+      options: {},
+    },
+  },
 });
